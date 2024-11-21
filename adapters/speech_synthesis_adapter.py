6c399b589a1b0fd8ace1d0b9e6c993d73e0f3eb1
thiagogre <thiagogre.dev@gmail.com>
2024-11-21 16:11:40 -0300

    add possibility to use gpu and change the model to jenny

--- adapters/speech_synthesis_adapter.py
+++ adapters/speech_synthesis_adapter.py
@@ -1,10 +1,12 @@
 from TTS.api import TTS
+import torch
 
 from application.ports import AbstractSpeechSynthesisPort
 
 """
     models:
     tts_models/en/ljspeech/vits--neon
+    tts_models/en/jenny/jenny
     tts_models/en/vctk/vits <- 256 M, 257 F, 270 F, 287 M, 293 F, 317 M, 360 F 
     
 """
@@ -12,14 +14,14 @@ from application.ports import AbstractSpeechSynthesisPort
 
 class SpeechSynthesisAdapter(AbstractSpeechSynthesisPort):
     def __init__(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.tts = TTS(
-            model_name="tts_models/en/vctk/vits",
+            model_name="tts_models/en/jenny/jenny",
             progress_bar=True,
-            gpu=False,
-        )
+        ).to(device=self.device)
 
     def execute(self, text: str, output_filename: str) -> None:
         try:
-            self.tts.tts_to_file(text=text, file_path=output_filename, speaker="p317")
+            self.tts.tts_to_file(text=text, file_path=output_filename)
         except Exception as e:
             raise RuntimeError(f"Failed to covert text to speech: {e}")
