94459e99d3ea3953a26e76c00360bc8de436bfb9
thiagogre <thiagogre.dev@gmail.com>
2024-11-21 15:36:04 -0300

    change model and add a description of some good female/male voices

--- adapters/speech_synthesis_adapter.py
+++ adapters/speech_synthesis_adapter.py
@@ -2,17 +2,24 @@ from TTS.api import TTS
 
 from application.ports import AbstractSpeechSynthesisPort
 
+"""
+    models:
+    tts_models/en/ljspeech/vits--neon
+    tts_models/en/vctk/vits <- 256 M, 257 F, 270 F, 287 M, 293 F, 317 M, 360 F 
+    
+"""
+
 
 class SpeechSynthesisAdapter(AbstractSpeechSynthesisPort):
     def __init__(self):
         self.tts = TTS(
-            model_name="tts_models/en/ljspeech/tacotron2-DDC",
+            model_name="tts_models/en/vctk/vits",
             progress_bar=True,
             gpu=False,
         )
 
     def execute(self, text: str, output_filename: str) -> None:
         try:
-            self.tts.tts_to_file(text=text, file_path=output_filename)
+            self.tts.tts_to_file(text=text, file_path=output_filename, speaker="p317")
         except Exception as e:
             raise RuntimeError(f"Failed to covert text to speech: {e}")
